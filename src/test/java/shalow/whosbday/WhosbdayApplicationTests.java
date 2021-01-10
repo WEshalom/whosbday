4658bde284f6353f9b581810cae2bd2b45c16bf5
Shalom Weiss <shalomweiss07@gmail.com>
2021-01-10 17:14:42 -0500

    Made new db and models and controllers with a many to many relationship. And deleted old files.

--- src/test/java/shalow/whosbday/WhosbdayApplicationTests.java
+++ src/test/java/shalow/whosbday/WhosbdayApplicationTests.java
@@ -1,21 +1,16 @@
 package shalow.whosbday;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
-import org.hibernate.criterion.Example;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import shalow.whosbday.models.Person;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,7 +30,7 @@ class WhosbdayApplicationTests {
 	@JsonFormat
 	private JSONObject personJsonObject;
 
-	private JacksonTester<Person>jsonObject;
+	//private JacksonTester<Person>jsonObject;
 
 
 	@Before("")
